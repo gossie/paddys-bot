7d7ad45dc96e22b4e2692550563f0b594620df94
André Schreck <andre.schreck@gmail.com>
2020-12-19 03:16:14 +0100

    #2: multiple choice

--- src/main/kotlin/com/github/gossie/paddysbot/SlackConfiguration.kt
+++ src/main/kotlin/com/github/gossie/paddysbot/SlackConfiguration.kt
@@ -29,7 +29,7 @@ class SlackConfiguration {
                                 .text(PlainTextObject.builder()
                                     .text(it.choice)
                                     .build())
-                                .actionId("choice-action")
+                                .actionId(it.id.toString())
                                 //.url("/choice")
                                 .build()
                         }
@@ -45,6 +45,7 @@ class SlackConfiguration {
             }
         }
 
+        app.
         app.blockAction("choice-action") { req, ctx ->
             ctx.respond("Deine Antwort war ${req.payload.actions[0].value}")
             ctx.ack()
