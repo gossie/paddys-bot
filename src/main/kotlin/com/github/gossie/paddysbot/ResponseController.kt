dcebd379f1c18e77a2bb011a2d476d35631f1982
André Schreck <andre.schreck@gmail.com>
2020-12-19 02:42:34 +0100

    #2: multiple choice

--- src/main/kotlin/com/github/gossie/paddysbot/ResponseController.kt
+++ src/main/kotlin/com/github/gossie/paddysbot/ResponseController.kt
@@ -3,6 +3,7 @@ package com.github.gossie.paddysbot
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller("/slack/response")
@@ -10,7 +11,7 @@ class ResponseController {
 
     @PostMapping("choice")
     @ResponseStatus(HttpStatus.OK)
-    fun handleResponse() {
+    fun handleResponse(@RequestBody body: Map<String, Object>) {
 
     }
 
